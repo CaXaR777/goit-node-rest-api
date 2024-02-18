1bcea035af37a8cd8b67b75be431b045da74dd0d
caxar777 <caxar7dam@gmail.com>
2024-02-19 00:39:13 +0200

    к

--- helpers/resizeImage.js
+++ helpers/resizeImage.js
@@ -7,7 +7,7 @@ const resizeImage = async (req, res, next) => {
   const { path } = req.file;
   const image = await Jimp.read(path);
   image.resize(250, 250);
-  await image.writeAsync(path);
+  await image.write(path);
   next();
 };
 
